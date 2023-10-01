0b7566f8c89dfc8cd7c42eb003ab75f777010586
Sourav Mondal <smsourav888@gmail.com>
2023-10-01 15:31:03 +0530

    Added solution3

--- solution3.js
+++ solution3.js
@@ -2,6 +2,5 @@ function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   
-  // Usage example
-  let randomNum = getRandomNumber(1, 100);
-  console.log(randomNum);
+  console.log("Random Number between 50 and 200:")
+  console.log(getRandomNumber(50, 200));
